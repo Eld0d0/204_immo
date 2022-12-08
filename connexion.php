7c05856e4ed3f8cec5e9d377c9ed0d1762d7e85e
Eld0d0 <nils.gill.arion@gmail.com>
2022-12-08 17:15:44 +0100

    Page de base

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Connexion</title>
+  <link rel="stylesheet" href="css/connexion.css">
+</head>
+<!-- On ajoute le header -->
+<?php include 'header.php';?>
+<body>
+
+</body>
+<!-- On ajoute le footer -->
+<?php include 'footer.php';?>
+</html>
