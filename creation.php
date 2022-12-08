99e4f07bbb3cfe27b0fcbba8f8bba24221d953c3
Eld0d0 <nils.gill.arion@gmail.com>
2022-12-08 17:16:53 +0100

    Ajout page de base

---
+++ creation.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="css/creation.css">
+</head>
+<!-- On ajoute le header -->
+<?php include 'header.php';?>
+<body>
+
+</body>
+<!-- On ajoute le footer -->
+<?php include 'footer.php';?>
+</html>
