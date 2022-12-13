1678eb3e8938bcf316186e38d508118fcb2ead94
Eld0d0 <nils.gill.arion@gmail.com>
2022-12-13 16:25:50 +0100

    Mise en forme

--- connexion.php
+++ connexion.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <title>Connexion</title>
   <link rel="stylesheet" href="css/connexion.css">
+  <link rel="stylesheet" href="css/normalize.css">
 </head>
 <!-- On ajoute le header -->
 <?php include 'header.php'; ?>
@@ -56,6 +57,8 @@
   if (array_key_exists('identifiant', $_POST) && array_key_exists('motdepasse', $_POST)
     && !empty($_POST['identifiant']) && !empty($_POST['motdepasse'])
   ) {
+
+
     // Mauvais identifiant ou mauvais mot de passe
     if ($_POST['identifiant'] !== "Administrateur" || $_POST['motdepasse'] !== "83CCutv8") {
       echo "Le login est incorrect ou le mot de passe est incorrect.";
@@ -68,7 +71,7 @@
   ?>
 
   <?php if (!connecte()) : /* Si l'utilisateur n'est pas connecté, on affiche le formulaire */ ?>
-    <form method="POST">
+      <form method="POST">
       <div class="row">
         <div class="col">
           <div class="form-group">
@@ -84,10 +87,13 @@
         </div>
         <div class="col">
           <br>
-          <button type="submit" class="btn btn-primary mb-2">Se connecter</button>
+          <div class="bouton_container">
+            <button type="submit" class="bouton_form">Se connecter</button>
+          </div>
         </div>
       </div>
     </form>
+    
 
   <?php else : /* Sinon, s'il est déjà connecté ou vient de se connecter, on affiche ce message (PLACEHOLDER, CE SERA À REMPLACER) */ ?>
     <p>Bienvenue <?php echo $_SESSION['identifiant']; ?>, votre mot de passe est <?php echo $_SESSION['motdepasse']; ?>.</p>
