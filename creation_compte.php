849482174132c50f52ef7712e71637dce2c29d3c
Eld0d0 <nils.gill.arion@gmail.com>
2022-12-14 16:48:02 +0100

    Préparation des pages

---
+++ creation_compte.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Création de compte</title>
+  <link rel="stylesheet" href="css/creation_compte_.css">
+  <link rel="stylesheet" href="css/normalize.css">
+
+  <!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<!-- On ajoute le header -->
+<?php include 'header.php'; ?>
+
+<body>
+
+
+</body>
+<!-- On ajoute le footer -->
+<?php include 'footer.php'; ?>
+<script src="js/index.js"></script>
+
+</html>
